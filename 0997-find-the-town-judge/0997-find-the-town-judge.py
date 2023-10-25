903fbfb76e0cb71953d2b2e960a29ca9a50650b1
Gopi Chand Pendyala <98773622+gpendyal@users.noreply.github.com>
2023-10-25 08:30:43 -0400

    Time: 818 ms (6.23%), Space: 21.7 MB (95.11%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -4,8 +4,11 @@ class Solution:
         a = [i[0] for i in trust]
         b = [i[1] for i in trust]
         
+        if len(set(a)) != n-1:
+            return -1
+        
         for i in range(1,n+1):
-            if b.count(i) == n-1 and len(set(a)) == n-1 and i not in a:
+            if b.count(i) == n-1 and i not in a:
                 return i
             
         return -1
