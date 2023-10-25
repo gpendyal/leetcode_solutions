3e023d529494ebfd79f35ddceb81d3a144ccb6ba
Gopi Chand Pendyala <98773622+gpendyal@users.noreply.github.com>
2023-10-25 08:30:00 -0400

    Time: 1122 ms (5.03%), Space: 21.9 MB (88.41%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        
+        a = [i[0] for i in trust]
+        b = [i[1] for i in trust]
+        
+        for i in range(1,n+1):
+            if b.count(i) == n-1 and len(set(a)) == n-1 and i not in a:
+                return i
+            
+        return -1
